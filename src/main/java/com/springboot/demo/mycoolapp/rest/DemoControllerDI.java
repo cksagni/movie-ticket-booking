db75547137308147c7c94cede7dce058fbed4cc0
Chandan Kumar <chandanagni91@gmail.com>
2024-08-09 12:35:45 +0530

    Dependency Injection:
    multiple implementation of an interface and using Primary & Qualifier annotation to mark 1 as lead

--- src/main/java/com/springboot/demo/mycoolapp/rest/DemoControllerDI.java
+++ src/main/java/com/springboot/demo/mycoolapp/rest/DemoControllerDI.java
@@ -2,6 +2,7 @@ package com.springboot.demo.mycoolapp.rest;
 
 import com.springboot.demo.mycoolapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoControllerDI {
     private Coach myCoach;
 
+//    use @Qualifier to give explicit info for DI --> public DemoControllerDI(@Qualifier("cricketCoach") Coach theCoach)
+//    use @Primary annotation to mark one class as primary in case of multiple implementation
+//    priority --> @Qualifier > @Primary
     @Autowired
-    public DemoControllerDI(Coach theCoach){
+    public DemoControllerDI(@Qualifier("trackCoach") Coach theCoach){
         myCoach = theCoach;
     }
+
+
+
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
