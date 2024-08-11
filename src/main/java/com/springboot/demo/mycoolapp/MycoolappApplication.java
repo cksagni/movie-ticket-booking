df118bb31645a927ea5ad751a4b5c4c9f5b2cc60
Chandan Kumar <chandanagni91@gmail.com>
2024-08-11 10:53:13 +0530

    Configure Spring Boot application to connect with MySQL

--- src/main/java/com/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,7 +1,9 @@
 package com.springboot.demo.mycoolapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(
 		scanBasePackages = {
@@ -15,4 +17,10 @@ public class MycoolappApplication {
 		SpringApplication.run(MycoolappApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
 }
