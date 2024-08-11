d6ad2952b7b12b5bedbc33e911f01c0718e21305
Chandan Kumar <chandanagni91@gmail.com>
2024-08-11 17:38:34 +0530

    Create a row in table using JPA

--- src/main/java/com/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,5 +1,7 @@
 package com.springboot.demo.mycoolapp;
 
+import com.springboot.demo.mycoolapp.dao.StudentDAO;
+import com.springboot.demo.mycoolapp.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,9 +20,28 @@ public class MycoolappApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
 			System.out.println("Hello World");
+
+			createMultipleStudent(studentDAO);
 		};
 	}
+
+	private void createMultipleStudent(StudentDAO studentDAO) {
+		System.out.println("Creating 3 student objects...");
+		Student tempStudent1 = new Student("John", "Doe", "john@xyz.com");
+		Student tempStudent2 = new Student("Jamie", "Reagan", "jamie@xyz.com");
+		Student tempStudent3 = new Student("Ryan", "Ivan", "ryan@xyz.com");
+
+		System.out.println("Saving the Students...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
+		System.out.println("Saved Student. Generated id: " + tempStudent1.getId());
+		System.out.println("Saved Student. Generated id: " + tempStudent2.getId());
+		System.out.println("Saved Student. Generated id: " + tempStudent3.getId());
+	}
+
 }
---
+++ src/main/java/com/springboot/demo/mycoolapp/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.springboot.demo.mycoolapp.dao;
+
+import com.springboot.demo.mycoolapp.entity.Student;
+
+public interface StudentDAO {
+    void save(Student student);
+}
